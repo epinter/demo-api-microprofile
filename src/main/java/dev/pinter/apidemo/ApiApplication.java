43ed24bb5d1ea463a56eab9e3f4e1e05f802e123
Emerson Pinter <e@pinter.dev>
2024-03-13 17:56:54 -0300

    Upgrade to Microprofile 6

--- src/main/java/dev/pinter/apidemo/ApiApplication.java
+++ src/main/java/dev/pinter/apidemo/ApiApplication.java
@@ -1,9 +1,19 @@
 package dev.pinter.apidemo;
 
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 @ApplicationPath("/")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Demo API Microprofile",
+                version = "0.1"
+        ),
+        servers = {@Server(url = "/", description = "localhost")}
+)
 public class ApiApplication extends Application {
 }
